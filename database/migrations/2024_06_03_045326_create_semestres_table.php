da4bbe0fd6663f1423a6acc0ae97981ae5393581
MlleOnana <104098231+MlleOnana@users.noreply.github.com>
2024-06-21 14:37:18 -0700

    semestre

--- database/migrations/2024_06_03_045326_create_semestres_table.php
+++ database/migrations/2024_06_03_045326_create_semestres_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('semestres', function (Blueprint $table) {
             $table->id();
             $table->string('numSemestre');
+            $table->integer('anneeDepart');
+            $table->integer('anneeFin');
+            $tble->integer('numSemaine');
             $table->timestamps();
         });
     }
